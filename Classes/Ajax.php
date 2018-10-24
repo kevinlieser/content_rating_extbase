d957b91140d10f78e801a431f08031247dc9ee19
Marianne Moser <mm@plan2.net>
2018-10-24 17:38:41 +0200

    [TASK] TYPO3 9 compatibility
    * Uses QueryBuilder for database connection
    * Adds composer.json

--- Classes/Ajax.php
+++ Classes/Ajax.php
@@ -3,6 +3,10 @@
 * Initialize Database
 */
 
+use TYPO3\CMS\Core\Database\Query\QueryBuilder;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Core\Database\ConnectionPool;
+
 class ajaxClass {
 
 	protected $hmacSalt = 'contentRatingExtbase_a3f60445f2031b5cd83534130eeba64cf4a0887b';
@@ -13,13 +17,16 @@ class ajaxClass {
 	private $rate_tmp3 = null;
 	
 	function main(){
-		
+
 		$rateUrl = \TYPO3\CMS\Core\Utility\GeneralUtility::_GP('url');
 		$rateUrlHash = \TYPO3\CMS\Core\Utility\GeneralUtility::_GP('urlhash');
 		$checkUrlHash = \TYPO3\CMS\Core\Utility\GeneralUtility::hmac($rateUrl, $this->hmacSalt);
+
+    /** @var QueryBuilder $queryBuilder */
+    $queryBuilder = GeneralUtility::makeInstance(ConnectionPool::class)->getQueryBuilderForTable('tx_contentrating_rates');
 		
 		if($checkUrlHash == $rateUrlHash) {
-			
+
 			// Maximum Rate value
 			$rateValue = intval(\TYPO3\CMS\Core\Utility\GeneralUtility::_GP('rate'));
 			if($rateValue < 0) { $rateValue = 0; }
@@ -28,54 +35,80 @@ class ajaxClass {
 			// Check IP
 			$selectFields = '*';
 			$fromTable    = 'tx_contentrating_rates';
-			$whereClause  = '1 AND rate_url = "'.$GLOBALS['TYPO3_DB']->quoteStr($rateUrl, 'tx_contentrating_rates').'" AND rate_ip = "'.$GLOBALS['TYPO3_DB']->quoteStr($_SERVER['REMOTE_ADDR'], 'tx_contentrating_rates').'"';
-			$groupBy      = '';
-			$orderBy      = '';
-			$limit        = '1';
-			$recordList = $GLOBALS['TYPO3_DB']->exec_SELECTgetRows($selectFields, $fromTable, $whereClause, $groupBy, $orderBy, $limit);
+			$limit        = 1;
+
+      /** @var \Doctrine\DBAL\Driver\Statement $statement */
+      $recordList = $queryBuilder
+        ->select($selectFields)
+        ->from($fromTable)
+        ->where(
+          $queryBuilder->expr()->eq('rate_url', $queryBuilder->createNamedParameter($rateUrl)),
+          $queryBuilder->expr()->eq('rate_ip', $queryBuilder->createNamedParameter($_SERVER['REMOTE_ADDR']))
+        )
+        ->setMaxResults($limit)
+        ->execute()->fetchAll();
+
+      $queryBuilder->resetQueryParts();
+
+
 			if(count($recordList) == 0) {
 				// Store to DB
 				$into_table   = 'tx_contentrating_rates';
-				$field_values = array('pid' => 1
-									 ,'rate_url' => $rateUrl
-									 ,'rate_ip' => $_SERVER['REMOTE_ADDR']
-									 ,'rate_value' => $rateValue
-									 ,'tstamp' => time()
-									 ,'crdate' => time()
-									 );
-									 
-				$res = $GLOBALS['TYPO3_DB']->exec_INSERTquery($into_table, $field_values);
-				$newUId = $GLOBALS['TYPO3_DB']->sql_insert_id();
+				$field_values = array(
+				  'pid' => 1,
+          'rate_url' => $rateUrl,
+          'rate_ip' => $_SERVER['REMOTE_ADDR'],
+          'rate_value' => $rateValue,
+          'tstamp' => time(),
+          'crdate' => time()
+				);
+
+        $queryBuilder
+          ->insert($into_table)
+          ->values($field_values)
+          ->execute();
+
 			} else {
 				// Update DB
 				$into_table   = 'tx_contentrating_rates';
-				$where_clause = 'rate_url = "'.$GLOBALS['TYPO3_DB']->quoteStr($rateUrl, 'tx_contentrating_rates').'" AND rate_ip = "'.$GLOBALS['TYPO3_DB']->quoteStr($_SERVER['REMOTE_ADDR'], 'tx_contentrating_rates').'"';
-				$field_values = array('rate_value' => $GLOBALS['TYPO3_DB']->quoteStr($rateValue, 'tx_contentrating_rates')
-									 ,'tstamp' => time()
-									 );
-				$res = $GLOBALS['TYPO3_DB']->exec_UPDATEquery($into_table, $where_clause, $field_values);
-				$cnt = $GLOBALS['TYPO3_DB']->sql_affected_rows();
-			}
-			
-			
+
+        $queryBuilder
+          ->update($into_table)
+          ->set('rate_value', $rateValue)
+          ->set('tstamp', time())
+          ->where(
+            $queryBuilder->expr()->eq('rate_url', $queryBuilder->createNamedParameter($rateUrl)),
+            $queryBuilder->expr()->eq('rate_ip', $queryBuilder->createNamedParameter($_SERVER['REMOTE_ADDR']))
+          )->execute();
+      }
+
 			// New Calc return
 			$selectFields = '*';
 			$fromTable    = 'tx_contentrating_rates';
-			$whereClause  = '1 AND rate_url = "'.$GLOBALS['TYPO3_DB']->quoteStr($rateUrl, 'tx_contentrating_rates').'"';
-			$groupBy      = '';
-			$orderBy      = '';
-			$limit        = '';
-			$recordList = $GLOBALS['TYPO3_DB']->exec_SELECTgetRows($selectFields, $fromTable, $whereClause, $groupBy, $orderBy, $limit);
-			$this->rate_count = count($recordList);
-			$GLOBALS['TYPO3_DB']->sql_num_rows($res);
-			foreach($recordList as $record) { $this->rate_tmp1 = $this->rate_tmp1 + $record['rate_value']; }
-			
-			// Calculate
-			$this->rate_tmp2 = $this->rate_count * 5; // Max possible Rating
-			$this->rate_tmp3 = round((100 / $this->rate_tmp2) * $this->rate_tmp1); // Percent Rating
-			
-			// Output
-			print $this->rate_tmp3;
+
+			$queryBuilder->resetQueryParts();
+
+      $recordList = $queryBuilder
+        ->select($selectFields)
+        ->from($fromTable)
+        ->where(
+          $queryBuilder->expr()->eq('rate_url', $queryBuilder->createNamedParameter($rateUrl))
+        )->execute()->fetchAll();
+
+      $this->rate_count = count($recordList);
+
+
+      foreach($recordList as $record) {
+          $this->rate_tmp1 = $this->rate_tmp1 + $record['rate_value'];
+      }
+
+      // Calculate
+      $this->rate_tmp2 = $this->rate_count * 5; // Max possible Rating
+      $this->rate_tmp3 = round((100 / $this->rate_tmp2) * $this->rate_tmp1); // Percent Rating
+
+      // Output
+      print $this->rate_tmp3;
+
 		}
 		
 	}
